771574376d48d711b677beb94759e4462bb20890
xiaoji <1290402981@qq.com>
2020-11-09 14:29:36 +0800

    add page

--- miniprogram/pages/aboutPage/package1/discover/discover.js
+++ miniprogram/pages/aboutPage/package1/discover/discover.js
@@ -1,93 +1,95 @@
-// miniprogram/pages/aboutPage/discover/discover.js
 const app = getApp()
+var isShow = false, animation, height = wx.getSystemInfoSync().windowHeight;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  },
+    StatusBar: app.globalData.StatusBar, //状态栏高度
+    CustomBar: app.globalData.CustomBar, //titleBar高度
+    hidden: true, //默认为隐藏
+    isShow,
+    //默认为圆形    宽高为设备高度÷15      
+		myStyle: "border-radius: " +
+			height + "px;height: " +
+			height / 15 + "px;width: " +
+			height / 15 + "px;",
+		nav: [{
+			navigation: [{
+					name: '动态',
+					src: '../../img/1.png'
+				},
+				{
+					name: '酷图',
+					src: '../../img/2.png'
+				},
+				{
+					name: '应用集',
+					src: '../../img/3.png'
+				},
+				{
+					name: '扫一扫',
+					src: '../../img/4.png'
+				},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+			],
+		}, {
+			navigation: [{
+					name: '分享',
+					src: '../../img/5.png'
+				},
+				{
+					name: '图文',
+					src: '../../img/6.png'
+				},
+				{
+					name: '提问',
+					src: '../../img/7.png'
+				},
+				{
+					name: '话题',
+					src: '../../img/8.png'
+        },
+      ],
+
+    }]
+  },
   onLoad: function (options) {
+    animation = wx.createAnimation({
+    duration: 300,
+    timingFunction: 'linear',
+  })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  tapRe: function(e) { 
-    console.log("tapRe") 
-    this.selectComponent("#custom").reBug() 
-    this.onLoad() 
-  }, 
-  tapFixBug: function(e) { 
-    console.log(this.selectComponent("#custom").data) 
-    wx.getSystemInfo({ 
-      success: e => { 
-        app.globalData.StatusBar = e.statusBarHeight; 
-        let capsule = wx.getMenuButtonBoundingClientRect(); 
-        if (capsule) { 
-          app.globalData.Custom = capsule; 
-          app.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight; 
-        } else { 
-          app.globalData.CustomBar = e.statusBarHeight + 50; 
-        } 
-      } 
-    }) 
-    this.selectComponent("#custom").fixBug() 
-    wx.showToast({ 
-      title: '如果刷新页面后仍未修复，请尝试重新进入小程序', 
-      icon: "none", 
-      duration: 3000 
-    }) 
-    wx.navigateTo({ 
-      url: '../algorithm/algorithm', 
-    }) 
-  }
+  onClickAdd: function(e) {
+		var menuStyle = ''
+		var that = this
+		that.animation = animation
+		that.setData({
+			hidden: false, //隐藏白色面板(ripple)
+			menuStyle: menuStyle, //设置底部加号按钮style
+		})
+		//判断是否显示
+		if (!isShow) {
+			//未显示 则执行动画 缩放设备高度÷15高度
+			that.animation.scale(height / 15).step()
+			//加号按钮执行打开动画
+			menuStyle = 'menuOpen'
+		} else {
+			//已显示 则执行动画 缩放回0
+			that.animation.scale(0).step()
+			//加号按钮执行关闭动画
+			menuStyle = 'menuClose'
+		}
+		isShow = !isShow //存储显示状态
+		that.setData({
+			animationData: that.animation.export(), //动画赋值
+			menuStyle: menuStyle, //加号按钮style赋值
+		})
+		//如果显示状态为true 延时200毫秒后执行内容显示 否则立即隐藏
+		isShow ?
+			setTimeout(function() {
+				that.setData({
+					isShow
+				})
+			}, 200) : that.setData({
+				isShow
+			})
+	},
 })
