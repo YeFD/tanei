a98558f9bcf8bfa85462d5935e7bf0dc6d88c8c9
Jkey104 <1043836038@qq.com>
2022-05-18 23:37:11 +0800

    remove

--- miniprogram/pages/aboutPage/package1/algorithm/components/3.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    newAb2Str: function(e) {
-        var r = new Uint8Array(e), n = String.fromCharCode.apply(null, r);
-        return decodeURIComponent(escape(n));
-    }
-};
